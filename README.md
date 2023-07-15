034dc8af08a3eb08e7cbac9946de59e1fa97570d
LeanWasTaken <kristsjavalds10@gmail.com>
2023-07-15 10:31:40 -0700

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# egg
-An egg counter discord bot
+# Egg Counter
+A discord bot to count the number of "egg" in a row in a specific channel. Dumb ig
+
+## How to run
+```
+npm i
+
+node index.js
+```
+make sure to rename .env.example to .env and fill it out beforehand.
+
+## How to set up the "DB"
+Create a data.json file and put this in
+
+```json
+{
+    "streak": 0,
+    "user": 0
+}
+
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,65 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+require('dotenv').config();
+
+const channelId = process.env.CHANNEL_ID;
+const token = process.env.TOKEN;
+const dataFile = 'data.json';
+
+let eggStreak = 0;
+let lastUser = '';
+let lastContent = '';
+
+const loadData = () => {
+  try {
+    const data = fs.readFileSync(dataFile);
+    const { streak, user } = JSON.parse(data);
+    eggStreak = streak;
+    lastUser = user;
+  } catch (error) {
+    console.error('Failed to load data:', error);
+  }
+};
+
+// Save data to the JSON file
+const saveData = () => {
+  const data = JSON.stringify({ streak: eggStreak, user: lastUser });
+  fs.writeFile(dataFile, data, (error) => {
+    if (error) {
+      console.error('Failed to save data:', error);
+    }
+  });
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  loadData();
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.channel.id === channelId && !message.author.bot) {
+    const content = message.content.toLowerCase();
+    const isEgg = content.includes('egg');
+
+    if (isEgg && (lastUser !== message.author.id || lastContent !== content)) {
+      eggStreak++;
+    } else if (!isEgg) {
+      eggStreak = 0;
+      message.channel.send(`${message.author} just broke the egg streak :/`);
+    } else if (lastUser === message.author.id) {
+      message.channel.send(`${message.author} just broke the egg streak :/. You cannot say 'egg' 2 times in a row.`);
+      eggStreak = 0;
+    }
+
+    lastUser = message.author.id;
+    lastContent = content;
+
+    saveData();
+    message.channel.setTopic(`Current egg streak: ${eggStreak}`)
+    console.log(`Current egg count: ${eggStreak}\nLast egger: ${lastUser}`)
+    console.log(`Data Saved!`)
+  }
+});
+
+client.login(token);
